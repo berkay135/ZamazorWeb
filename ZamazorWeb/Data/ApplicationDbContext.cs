1231202100040b2b81b96eda6bef6ae121034917
berkay135 <mlfgsm@gmail.com>
2024-05-18 17:30:50 +0300

    -dummy data seeded to db

--- ZamazorWeb/Data/ApplicationDbContext.cs
+++ ZamazorWeb/Data/ApplicationDbContext.cs
@@ -13,5 +13,15 @@ namespace ZamazorWeb.Data
 
         //Create table named "Categories"
         public DbSet<Category> Categories { get; set; }
+
+        //Seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1},
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
 }
