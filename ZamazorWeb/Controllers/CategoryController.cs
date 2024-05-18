4132088927f8a39799e95caab8c1f6b72209c822
berkay135 <mlfgsm@gmail.com>
2024-05-18 18:36:53 +0300

    -retrieved categories and passes object to view

--- ZamazorWeb/Controllers/CategoryController.cs
+++ ZamazorWeb/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ZamazorWeb.Data;
+using ZamazorWeb.Models;
 
 namespace ZamazorWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        //Implementation of ApplicatonDbContext
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            //Retrieve categories from sql server and pass object to view
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
