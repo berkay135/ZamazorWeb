8ec826bd2abf2e571f120e41e50eb2d717db28ba
berkay135 <mlfgsm@gmail.com>
2024-05-18 16:51:02 +0300

    -added model category -configured connection string to datebase -added required packages -added data folder

---
+++ ZamazorWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZamazorWeb.Data
+{
+    //Congiguration for entity framework
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+    }
+}
