7f0cc43342fa84595e36906d17b7c800c72331a3
berkay135 <mlfgsm@gmail.com>
2024-05-18 17:01:50 +0300

    -add-migration AddCategoryTableToDb

--- ZamazorWeb/Data/ApplicationDbContext.cs
+++ ZamazorWeb/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ZamazorWeb.Models;
 
 namespace ZamazorWeb.Data
 {
-    //Congiguration for entity framework
+    //Congiguration for entity framework core
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
                
         }
+
+        //Create table named "Categories"
+        public DbSet<Category> Categories { get; set; }
     }
 }
