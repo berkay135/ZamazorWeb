89f54137d42f156beef71b280a2f87b1970c163a
berkay135 <mlfgsm@gmail.com>
2024-05-19 15:28:08 +0300

    -delete and edit buttons added

--- ZamazorWeb/Controllers/CategoryController.cs
+++ ZamazorWeb/Controllers/CategoryController.cs
@@ -32,18 +32,83 @@ namespace ZamazorWeb.Controllers
 		public IActionResult Create(Category obj)
 		{
 			//server side validation
-			//if (obj.Name == obj.DisplayOrder.ToString())
-            //{
+			if (obj.Name == obj.DisplayOrder.ToString())
+            {
                 //ModelState.AddModelError("name","The Display Order cannot exactyl match the Name.");
-            //}
+            }
+
 			if (ModelState.IsValid)
             {
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"]= "Category created successfully";
 				return RedirectToAction("Index");
 			}
             return View();
+		}
+		//implementation of Edit button
+		//GET
+		public IActionResult Edit(int? id) //we need id user wants to edit
+		{
+			if (id== null || id == 0) 
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+			//Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
 			
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+
+		//POST
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		//implementation of Delete button
+		//GET
+		public IActionResult Delete(int? id) //we need id user wants to edit
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+			//Category? categoryFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+
+		//POST
+		[HttpPost,ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
 		}
 	}
 }
