41cb147de28cf7fa4d7c987296bafdf013a7ee15
berkay135 <mlfgsm@gmail.com>
2024-05-23 21:11:27 +0300

    -before identity scafold

--- Zamazor.DataAccess/Data/ApplicationDbContext.cs
+++ Zamazor.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Zamazor.Models;
 
 namespace ZamazorWeb.DataAccess.Data
 {
     //Configuration for entity framework core
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
@@ -19,6 +20,7 @@ namespace ZamazorWeb.DataAccess.Data
         //Seed data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1},
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
